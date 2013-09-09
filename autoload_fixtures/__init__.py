9bc38dd61d0bc0c62d51f5738ef43dbe86d9c48e
Luan fonseca de farias <luanfonceca@gmail.com>
2013-09-09 19:49:25 -0300

    Releasing the 0.2 version! :)

--- autoload_fixtures/__init__.py
+++ autoload_fixtures/__init__.py
@@ -4,7 +4,7 @@
 """
 autoload_fixtures/__init__.py
 
-Created by Luan Fonseca <luanfonceca@gmail.com> on 2013-08-08
+Created by Luan Fonseca <luanfonceca@gmail.com> on 2013-09-08
 """
 
 from django.dispatch import receiver
@@ -13,8 +13,8 @@ from django.db.models.loading import get_models, get_app
 from south.signals import post_migrate
 
 @receiver(post_migrate)
-def loadd_fixtures_for_app(sender, **kwargs):
-    app_name = kwargs.get('app')
+def load_fixtures_for_app(sender, **kwargs):
+    app_name = kwargs.get("app")
     if not app_name: return
 
     print u"Running autoload fixtures for %s:" % app_name
@@ -22,7 +22,7 @@ def loadd_fixtures_for_app(sender, **kwargs):
         model_name = model.__name__.lower()
         print u" > %s:%s" % (app_name, model_name)
         fixture_path = "%(app_name)s/fixtures/%(model_name)s.json" % {
-            'app_name': app_name,
-            'model_name': model_name
+            "app_name": app_name,
+            "model_name": model_name
         }
-        call_command('loaddata', fixture_path)
+        call_command("loaddata", fixture_path)
--- setup.py
+++ setup.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
+# encoding: utf-8
+
 from setuptools import setup
 
 try:
-	long_description = file('README.rst').read()
+    long_description = file('README.rst').read()
 except IOError:
-	long_description = ''
+    long_description = ''
 
 setup(
     name='django-autoload-fixtures',
-    version='0.1.6',
+    version='0.2',
     url='https://github.com/luanfonceca/django-autoload-fixtures',
     license='BSD',
     description='This library allows you to load initial Fixtures automagically after the South migrate your Model.',
@@ -30,8 +31,8 @@ setup(
         'autoload_fixtures',
     ],
     install_requires=[
-    	'setuptools',
-    	'Django>=1.3',
-    	'south',
+        'setuptools',
+        'Django>=1.3',
+        'south',
     ],
 )
